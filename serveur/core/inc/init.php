e82b0182dd1a1266330bce42ed1ae7ecc344f78c
kyxalia <kyxalia@c101d488-4eec-c0fd-ef62-12dd602b6c3e>
2013-05-22 15:14:31 +0000

    Modif système logs

--- serveur/core/inc/init.php
+++ serveur/core/inc/init.php
@@ -2,11 +2,14 @@
 
 // Ouverture du serveur
 
-function thread_server() {
+function thread_server($res) {
 	Server::getInstance();
+	exit($res);
 }
 
 $thread_server = new Thread('thread_server');
-$thread_server->start();
+$thread_server->start(1);
 
 GameManager::getInstance()->createGame();
+
+while ($thread_server->isAlive(1));
