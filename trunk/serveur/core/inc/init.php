e7749276cf75ffe8a4f5332ddeffcfd257083cd1
kyxalia <kyxalia@c101d488-4eec-c0fd-ef62-12dd602b6c3e>
2013-05-24 13:29:08 +0000

    Debug int

--- trunk/serveur/core/inc/init.php
+++ trunk/serveur/core/inc/init.php
@@ -2,25 +2,18 @@
 
 // Ouverture du serveur
 
-$b1 = Bal::create();
-$b2 = Bal::create();
-Server::getInstance($b1->getId(), $b2->getId());
-
-/*
-function thread_server($res) {
-	
+function thread_server($res, $b1, $b2) {
+	Server::getInstance($b1->getId(), $b2->getId());
 	exit($res);
 }
 
-//$thread_server = new Thread('thread_server');
-//$thread_server->start(1);
 
-//GameManager::getInstance()->createGame();
+$b1 = Bal::create();
+$b2 = Bal::create();
 
-$b = Bal::create(1);
-$b->write("Packet1");
-$b->write("Packet2");
-echo $b->read();
+$thread_server = new Thread('thread_server');
+$thread_server->start(1, $b1, $b2);
 
-//while ($thread_server->isAlive(1));
-*/
+GameManager::getInstance()->createGame();
+
+while ($thread_server->isAlive(1));
