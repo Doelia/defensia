705713e4d0c794fbf48d3ca047e9e9b244c3f3f2
Znoraka <Znoraka@c101d488-4eec-c0fd-ef62-12dd602b6c3e>
2013-05-28 08:09:33 +0000

    fix compatibilite

--- trunk/serveur/core/inc/lib/objets/level/tower/TowerTemplate.class.php
+++ trunk/serveur/core/inc/lib/objets/level/tower/TowerTemplate.class.php
@@ -5,28 +5,28 @@ class TowerTemplate
 
 	public static $FAST_TOWER_TYPE = "fastTower";
 	public static $FAST_TOWER_PRICE = 50;
-	public static $FAST_TOWER_TEMPLATE = [
+	public static $FAST_TOWER_TEMPLATE = array(
 											"damage" => 10,
 											"fireRate" => 10,
 											"range" => 10,
 											"radius" => 1
-										];
+										);
 
 	public static $SLOW_TOWER_TYPE = "slowTower";
 	public static $SLOW_TOWER_PRICE = 50;
-	public static $SLOW_TOWER_TEMPLATE = [
+	public static $SLOW_TOWER_TEMPLATE = array(
 											"damage" => 50,
 											"fireRate" => 3,
 											"range" => 10,
 											"radius" => 1
-										];	
+										);	
 
 	public static $AOE_TOWER_TYPE = "AOETower";
 	public static $AOE_TOWER_PRICE = 50;
-	public static $AOE_TOWER_TEMPLATE = [
+	public static $AOE_TOWER_TEMPLATE = array(
 											"damage" => 50,
 											"fireRate" => 10,
 											"range" => 10,
 											"radius" => 5
-										];																	
+				);																	
 }
