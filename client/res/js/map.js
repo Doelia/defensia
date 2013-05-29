58328b518ede033603f7f9de2b3c55f96a2ddcae
manuel.cha34@gmail.com <manuel.cha34@gmail.com@c101d488-4eec-c0fd-ef62-12dd602b6c3e>
2013-05-29 14:15:24 +0000

    Ajout nouvelles textures route dans le css

--- client/res/js/map.js
+++ client/res/js/map.js
@@ -8,8 +8,14 @@ function loadMap() {
 		    	],
 		    	
 		   "chemin": [
-		    		{ "x": 1, "y": 2, "texture": "gd" },
-		            { "x": 2, "y": 3, "texture": "gh" }
+		    		{ "x": 1, "y": 1, "texture": "bd" },
+		            { "x": 2, "y": 1, "texture": "gd" },
+		            { "x": 3, "y": 1, "texture": "gb" },
+		            { "x": 1, "y": 2, "texture": "bh" },
+		            { "x": 3, "y": 2, "texture": "bh" },
+		            { "x": 1, "y": 3, "texture": "hd" },
+		            { "x": 2, "y": 3, "texture": "gd" },
+		            { "x": 3, "y": 3, "texture": "gh" }
 		    	],
 		    	
 
