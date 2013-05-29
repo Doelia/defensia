8778c68dbff746280d88f80d4487bb2b93f92993
kyxalia <kyxalia@c101d488-4eec-c0fd-ef62-12dd602b6c3e>
2013-05-29 13:15:23 +0000

    Modifs diverses

---
+++ client/res/js/Objets/Cellule.js
@@ -0,0 +1,37 @@
+
+/*
+	Ne gére pas le graphique
+	Sert uniquement pour les calculs, detections d'objet, colisions...
+*/
+function Cellule(x, y)
+{
+	this.x = x;
+	this.y = y;
+	this.objectOn = null; // Objet jquery placé sur la case
+
+	this.onconstruct = function()
+	{
+		
+	}
+
+	/*
+		A appeller lors d'un changement
+	*/
+	this.setObjectOn = function(objectJquery)
+	{
+		this.objectOn = objectJquery;
+	}
+
+	/*
+		Retourne true si l'élement sur la case est un monstre
+		Stratégie : Utilisation du nom de la balise
+	*/
+	this.haveAMonstre = function()
+	{
+		
+	}
+
+
+	this.onconstruct();
+	
+}
--- client/res/js/Objets/Game.js
+++ client/res/js/Objets/Game.js
@@ -1,8 +1,8 @@
 function Game()
 {
 	console.log("Game.construct()")
+	
 	this.plateau = new Plateau();
 	this.plateau.preparePlateau();
 
-	this.
 }
